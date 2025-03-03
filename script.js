e351eec84226b00f40383d73cdbacf77e68cac73
BiniGar <jscsolano22@src.pshs.edu.ph>
2025-03-03 11:51:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function messageFunction (){
  let messageVerify = prompt('Enter the message: '); 
-  if (messageVerify.indexOf("aI") >= 0 || messageVerify.indexOf("Ai") >= 0){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is fake!'; } 
-   else if (messageVerify.indexOf("FR") >= 0 ){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is legitimate!'; } 
-   else if (messageVerify.indexOf("FR") >= 0 && messageVerify.indexOf("AI") >= 0){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is legitimate!'; } 
-   else if (messageVerify.indexOf("AI") >= 0){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is fake!'; } 
+  if (messageVerify.indexOf("aI") >= 0 || messageVerify.indexOf("Ai") >= 0){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is fake!'; 
+  } else if (messageVerify.indexOf("FR") >= 0 ){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is legitimate!';
+  } else if (messageVerify.indexOf("FR") >= 0 && messageVerify.indexOf("AI") >= 0){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is legitimate!';
+  } else if (messageVerify.indexOf("AI") >= 0){ document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is fake!'; } 
     else { document.getElementById('result').innerHTML = 'The message "' + messageVerify + '" is not yet encoded!'; } }
